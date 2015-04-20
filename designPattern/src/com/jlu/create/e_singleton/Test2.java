df8814175d8f5d15a7f3a2b30eb6f89dd0509dd6
pan <pan@pan-PC>
2015-04-20 21:16:52 +0800

    aaaaa

---
+++ designPattern/src/com/jlu/create/e_singleton/Test2.java
@@ -0,0 +1,14 @@
+package com.jlu.create.e_singleton;
+/**
+ * 这是测试2的类
+ * @title Test2
+ * @description TODO 
+ * @author pan
+ * @date 2015年4月20日
+ * @version 1.0
+ */
+public class Test2 {
+	
+	
+
+}
